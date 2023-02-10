828415605f5b277101e830e4b68a875d82d09e6e
Anton Zakharov <c0rnota@gmail.com>
2023-02-11 00:37:01 +0300

    WIP fix deadlock

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
-	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
-	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
 	"os"
 )
+
+const providerName = "regru-dns"
 
 var (
 	GroupName = os.Getenv("GROUP_NAME")
@@ -38,48 +38,47 @@ type regruDNSProviderConfig struct {
 }
 
 func (c *regruDNSProviderSolver) Name() string {
-	return "regru-dns"
+	return providerName
 }
 
 func (c *regruDNSProviderSolver) Present(challengeRequest *v1alpha1.ChallengeRequest) error {
-	klog.Infof("call function Present: namespace=%s, zone=%s, fqdn=%s", challengeRequest.ResourceNamespace, challengeRequest.ResolvedZone, challengeRequest.ResolvedFQDN)
-	cfg, err := loadConfig(challengeRequest.Config)
-	if err != nil {
-		return fmt.Errorf("unable to load config: %v", err)
-	}
-
-	klog.Infof("decoded configuration %v", cfg)
+	klog.Infof("Call function Present: namespace=%s, zone=%s, fqdn=%s", challengeRequest.ResourceNamespace, challengeRequest.ResolvedZone, challengeRequest.ResolvedFQDN)
+	//_, err := loadConfig(challengeRequest.Config)
+	//if err != nil {
+	//	return fmt.Errorf("unable to load config: %v", err)
+	//}
+	//
+	//klog.Infof("decoded configuration %v", cfg)
 
 	regruClient := NewRegruClient(regru.username, regru.password, getDomainFromZone(challengeRequest.ResolvedZone))
 
 	klog.Infof("present for entry=%s, domain=%s, key=%s", challengeRequest.ResolvedFQDN, getDomainFromZone(challengeRequest.ResolvedZone), challengeRequest.Key)
 
-	err = regruClient.createTXT(challengeRequest.ResolvedFQDN, challengeRequest.Key)
-	if err != nil {
-		return fmt.Errorf("unable to check TXT record: %v", err)
+	if err := regruClient.createTXT(challengeRequest.ResolvedFQDN, challengeRequest.Key); err != nil {
+		return fmt.Errorf("unable to create TXT record: %v", err)
 	}
+
 	return nil
 }
 
 func (c *regruDNSProviderSolver) CleanUp(challengeRequest *v1alpha1.ChallengeRequest) error {
-	klog.Infof("call function CleanUp: namespace=%s, zone=%s, fqdn=%s",
+	klog.Infof("Call function CleanUp: namespace=%s, zone=%s, fqdn=%s",
 		challengeRequest.ResourceNamespace, challengeRequest.ResolvedZone, challengeRequest.ResolvedFQDN)
-	cfg, err := loadConfig(challengeRequest.Config)
-	if err != nil {
-		return fmt.Errorf("unable to load config: %v", err)
-	}
-
-	klog.Infof("decoded configuration %v", cfg)
+	//cfg, err := loadConfig(challengeRequest.Config)
+	//if err != nil {
+	//	return fmt.Errorf("unable to load config: %v", err)
+	//}
+	//
+	//klog.Infof("decoded configuration %v", cfg)
 
 	regruClient := NewRegruClient(regru.username, regru.password, getDomainFromZone(challengeRequest.ResolvedZone))
-	klog.Infof("present for entry=%s, domain=%s, key=%s", challengeRequest.ResolvedFQDN, getDomainFromZone(challengeRequest.ResolvedZone), challengeRequest.Key)
+	klog.Infof("delete entry=%s, domain=%s, key=%s", challengeRequest.ResolvedFQDN, getDomainFromZone(challengeRequest.ResolvedZone), challengeRequest.Key)
 
-	err = regruClient.deleteTXT(challengeRequest.ResolvedFQDN, challengeRequest.Key)
-	if err != nil {
-		return fmt.Errorf("unable to check TXT record: %v", err)
+	if err := regruClient.deleteTXT(challengeRequest.ResolvedFQDN, challengeRequest.Key); err != nil {
+		return fmt.Errorf("unable to delete TXT record: %v", err)
 	}
-	return nil
 
+	return nil
 }
 
 func (c *regruDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, _ <-chan struct{}) error {
@@ -92,18 +91,18 @@ func (c *regruDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, _ <-c
 	return nil
 }
 
-func loadConfig(cfgJSON *extapi.JSON) (regruDNSProviderConfig, error) {
-	cfg := regruDNSProviderConfig{}
-	if cfgJSON == nil {
-		return cfg, nil
-	}
-
-	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
-		klog.Errorf("error decoding solver config: %v", err)
-		return cfg, fmt.Errorf("error decoding solver config: %v", err)
-	}
-	return cfg, nil
-}
+//func loadConfig(cfgJSON *extapi.JSON) (regruDNSProviderConfig, error) {
+//	cfg := regruDNSProviderConfig{}
+//	if cfgJSON == nil {
+//		return cfg, nil
+//	}
+//
+//	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
+//		klog.Errorf("error decoding solver config: %v", err)
+//		return cfg, fmt.Errorf("error decoding solver config: %v", err)
+//	}
+//	return cfg, nil
+//}
 
 // getDomainFromZone returns domain name from ResolvedZone without last dot.
 func getDomainFromZone(zone string) string {
